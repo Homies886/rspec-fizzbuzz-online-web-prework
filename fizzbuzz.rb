6a95bffd5319e74b5d99acae401e2ed7d96e3af7
fortunate-partial-4781 <niko876@hotmail.com>
2019-02-07 02:14:11 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,9 @@
-def fizzbuzz
-  
+def fizzbuzz(int)
+  if (int % 3 == true) && (int % 5 == true)
+    return "FizzBuzz"
+  elsif int % 3 == true
+    return "Fizz"
+  elsif int % 5 == true
+    return "Buzz"
+  end
 end
