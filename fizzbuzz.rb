3dfb500e9310377c2dff9187ea6906840e741a5c
fortunate-partial-4781 <niko876@hotmail.com>
2019-02-07 02:18:40 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
-  if ((int % 3) == true) && (int % 5 == true)
+  if (int % 3 == 0) && (int % 5 == 0)
     return "FizzBuzz"
-  elsif (int % 3) == true
+  elsif int % 3 == 0
     return "Fizz"
-  elsif (int % 5) == true
+  elsif int % 5 == 0
     return "Buzz"
   end
 end
